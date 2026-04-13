5640ad2f6af42faba8eb3795097c4ab3e543951c
orangeflyfly <toyotamark02@gmail.com>
2026-04-13 18:27:39 +0800

    Create pokemonData.js

---
+++ pokemonData.js
@@ -0,0 +1,10 @@
+// 這裡是機台的「專屬資料庫」，未來新增寶可夢都在這裡加！
+const machineInventory = [
+    { id: 1, name: "小拉達", rarity: 1, icon: "🐭" },
+    { id: 2, name: "波波", rarity: 1, icon: "🐦" },
+    { id: 3, name: "皮卡丘", rarity: 3, icon: "⚡" },
+    { id: 4, name: "噴火龍", rarity: 4, icon: "🔥" },
+    { id: 5, name: "水箭龜", rarity: 4, icon: "🐢" },
+    { id: 6, name: "超夢", rarity: 5, icon: "🔮" }
+    // 如果要新增第 7 隻，就照上面的格式寫在下面，用逗號隔開
+];
